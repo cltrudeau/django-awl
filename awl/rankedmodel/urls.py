20e804fc20242fc3f12c7d1feed1af6008cac2de
Christopher Trudeau <ctrudeau@arsensa.com>
2016-08-04 13:05:46 -0400

    Fix missing ^ in url patterns

--- awl/rankedmodel/urls.py
+++ awl/rankedmodel/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('awl.rankedmodel.views',
-    url(r'move/(\d+)/(\d+)/(\d+)/$', 'move', name='awl-rankedmodel-move'),
+    url(r'^move/(\d+)/(\d+)/(\d+)/$', 'move', name='awl-rankedmodel-move'),
 )
--- awl/tests/urls.py
+++ awl/tests/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from awl.rankedmodel import urls as ranked_urls
 
 urlpatterns = [
-    url(r'admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
-    url(r'rankedmodel/', include(ranked_urls)),
+    url(r'^rankedmodel/', include(ranked_urls)),
 
-    url(r'awl_test_views/test_view_for_messages/$', 
+    url(r'^awl_test_views/test_view_for_messages/$', 
         'awl.tests.views.test_view_for_messages', ),
 ]
